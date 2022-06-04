e43da0af671990090dd8ec9098854a548a80ad8d
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-04 18:38:38 +0300

    Changed the script to add counter for GET

--- counter-service.py
+++ counter-service.py
@@ -1,14 +1,20 @@
 #!flask/bin/python
 from flask import Flask, request, request_started
 app = Flask(__name__)
-counter = 0
+counter_for_post = 0
+counter_for_get = 0
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global counter
+    global counter_for_post
+    global counter_for_get
     if request.method == "POST":
-        counter+=1
-        return "Hmm, Plus 1 please "
+        counter_for_post+=1
+        return str(f"Another Post Request has been made but the counter for GET Requset is: {counter_for_get}")
+    elif request.method == "GET":
+        counter_for_get+=1
+        return str(f"Another GET Request has been made but the counter for Post request is: {counter_for_post} ")
     else:
-        return str(f"The counter is: {counter} ")
+        return str(f"the counter for GET Requset is: {counter_for_get} and the counter for Post request is: {counter_for_post} ")
+
 if __name__ == '__main__':
     app.run(debug=True,port=80,host='0.0.0.0') 
